d14aa357dddeda39254b2b61bcacbdcfdba247aa
geoenterprise <jovannyrey@hotmail.com>
2025-03-19 22:34:05 -0500

    Week 3 learning activities

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const env = require('dotenv').config();
 const app = express();
 const static = require('./routes/static');
 const expressLayouts = require('express-ejs-layouts');
+const baseController = require('./controllers/baseController');
+const inventoryRoute = require('./routes/inventoryRoute');
 
 /* ***********************
  * View Engine and Templates
@@ -23,10 +25,14 @@ app.set('layout', './layouts/layout');
  *************************/
 app.use(static);
 
-// Index route
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
-});
+// Index route-----------
+// app.get('/', (req, res) => {
+//   res.render('index', { title: 'Home' });
+// });
+app.get('/', baseController.buildHome);
+
+// Inventory routes
+app.use('/inv', inventoryRoute);
 
 /* ***********************
  * Local Server Information
